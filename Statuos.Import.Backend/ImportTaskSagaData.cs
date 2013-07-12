55437a401820447b382dbfb9ebcab11616d61329
joshuadeanhall@gmail.com <joshuadeanhall@gmail.com>
2013-07-11 23:04:20 -0400

    Prepared the project for using Sagas for Bulk Task Imports.

---
+++ Statuos.Import.Backend/ImportTaskSagaData.cs
@@ -0,0 +1,17 @@
+﻿using NServiceBus.Saga;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Statuos.Import.Backend
+{
+    public class ImportTaskSagaData : ContainSagaData
+    {
+        [Unique]
+        public Guid ImportId { get; set; }
+        public int ClientsToBeImported { get; set; }
+        public int ClientsImported { get; set; }
+        public int ClientsFailedToImport { get; set; }
+    }
+}
---
+++ Statuos.Import.Backend/ImportTasksSaga.cs
@@ -0,0 +1,59 @@
+﻿using NServiceBus;
+using NServiceBus.Logging;
+using NServiceBus.Saga;
+using Statuos.Messages;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Statuos.Import.Backend
+{
+    public class ImportTasksSaga : Saga<ImportTaskSagaData>, 
+                                    IAmStartedByMessages<ImportTasksMessage>, 
+                                    IHandleMessages<ITaskImport>, 
+                                    IHandleMessages<ITaskImportFailed>, 
+                                    IHandleTimeouts<ImportTaskTimeout>
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(ImportTasksSaga));
+
+        public override void ConfigureHowToFindSaga()
+        {
+            ConfigureMapping<ITaskImport>(s => s.ImportId, m => m.ImportId);
+            ConfigureMapping<ITaskImportFailed>(s => s.ImportId).ToSaga(s => s.ImportId);
+        }
+
+        public void Handle(ImportTasksMessage message)
+        {
+            RequestTimeout(TimeSpan.FromHours(24), new ImportTaskTimeout());
+            var lines = File.ReadAllLines(message.FileLocation);
+            var csv = from line in lines
+                      select (from piece in line
+                              select piece.Split(',')).ToArray();
+
+            foreach (var csvLine in csv)
+            {
+                this.Data.ClientsToBeImported++;
+                //Create Client
+                //Create Project
+                //Create Task
+            }
+        }
+
+        public void Handle(ITaskImport message)
+        {
+            this.Data.ClientsImported++;
+        }
+
+        public void Handle(ITaskImportFailed message)
+        {
+            this.Data.ClientsFailedToImport++;
+        }
+
+        public void Timeout(ImportTaskTimeout state)
+        {
+            //Check status.  If not all tasks finished record to database.
+        }
+
+
+    }
+}
